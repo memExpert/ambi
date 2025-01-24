b1115610015fa557b52f926e7f85e7990d671d0b
Maksim Hanchar <pottermax2000@gmail.com>
2025-01-24 22:51:44 +0100

    REWORK begin of rework (simplified)

--- src/ambi_daemon.c
+++ src/ambi_daemon.c
@@ -1,58 +1,370 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <time.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <getopt.h>
+#include <limits.h>
+#include <pthread.h>
 
-#include "../include/config.h"
 
-void handle_client(int client_fd) {
-    char buffer[256];
-    read(client_fd, buffer, sizeof(buffer));
-    printf("Command received: %s\n", buffer);
-    close(client_fd);
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/extensions/XShm.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#include "daemon.h"
+
+
+
+
+
+static const char *help_str =
+        " Daemon name:  " DAEMON_NAME          "\n"
+        " Daemon  ver:  " DAEMON_VERSION_STR   "\n\n"
+#ifdef  DEBUG
+        " Build  mode:  debug\n"
+#else
+        " Build  mode:  release\n"
+#endif
+        " Build  date:  " __DATE__ "\n"
+        " Build  time:  " __TIME__ "\n"
+#if  CHANGED_FILES == 0
+        " Build  hash:  "  COMMIT_HASH "\n\n"
+#else
+        " Build  hash:  *" COMMIT_HASH "\n\n"
+#endif
+        "Options:                      description:\n\n"
+        "       --no_chdir             Don't change the directory to '/'\n"
+        "       --no_fork              Don't do fork\n"
+        "       --no_close             Don't close standart IO files\n"
+        "       --pid_file [value]     Set pid file name\n"
+        "       --log_file [value]     Set log file name\n"
+        "       --cmd_pipe [value]     Set CMD Pipe name\n"
+        "  -v,  --version              Display daemon version\n"
+        "  -h,  --help                 Display this help\n\n";
+
+
+
+// indexes for long_opt function
+enum
+{
+    cmd_opt_help    = 'h',
+    cmd_opt_version = 'v',
+
+    //daemon options
+    cmd_opt_no_chdir,
+    cmd_opt_no_fork,
+    cmd_opt_no_close,
+    cmd_opt_pid_file,
+    cmd_opt_log_file,
+    cmd_opt_cmd_pipe
+};
+
+
+
+static const char *short_opts = "hv";
+
+
+static const struct option long_opts[] =
+{
+    { "version",      no_argument,       NULL, cmd_opt_version  },
+    { "help",         no_argument,       NULL, cmd_opt_help     },
+
+    //daemon options
+    { "no_chdir",     no_argument,       NULL, cmd_opt_no_chdir },
+    { "no_fork",      no_argument,       NULL, cmd_opt_no_fork  },
+    { "no_close",     no_argument,       NULL, cmd_opt_no_close },
+    { "pid_file",     required_argument, NULL, cmd_opt_pid_file },
+    { "log_file",     required_argument, NULL, cmd_opt_log_file },
+    { "cmd_pipe",     required_argument, NULL, cmd_opt_cmd_pipe },
+
+    { NULL,           no_argument,       NULL,  0 }
+};
+
+
+
+pthread_t  pthread_cmd_pipe;
+
+
+
+
+void daemon_exit_handler(int sig)
+{
+
+    //Here we release resources
+
+
+    unlink(daemon_info.pid_file);
+    unlink(daemon_info.cmd_pipe);
+
+    _exit(EXIT_FAILURE);
 }
 
-int main() {
-    struct tm* tm_ptr;
-    time_t current_time;
 
 
-    int server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        perror("Error: socket is not created.");
-        return 1;
-    }
+void init_signals(void)
+{
+    set_sig_handler(SIGINT,  daemon_exit_handler); //for Ctrl-C in terminal for debug (in debug mode)
+    set_sig_handler(SIGTERM, daemon_exit_handler);
 
-    struct sockaddr_un addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
-    unlink(SOCKET_PATH);
+    set_sig_handler(SIGCHLD, SIG_IGN); // ignore child
+    set_sig_handler(SIGTSTP, SIG_IGN); // ignore tty signals
+    set_sig_handler(SIGTTOU, SIG_IGN);
+    set_sig_handler(SIGTTIN, SIG_IGN);
+    set_sig_handler(SIGHUP,  SIG_IGN);
+}
 
-    if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-        perror("Error: can't link socket.\n");
-        return 1;
-    }
 
-    listen(server_fd, 5);
-    printf("ambi_daemon started successfully.\n");
 
-    while (1) {
-        int client_fd = accept(server_fd, NULL, NULL);
-        if (client_fd < 0) {
-            perror("Error accept");
-            continue;
+void processing_cmd(int argc, char *argv[])
+{
+    int opt;
+
+    // We use the processing_cmd function for processing the command line and
+    // for commands from the DAEMON_CMD_PIPE_NAME
+    // For this we use the getopt_long function several times
+    // to work properly, we must reset the optind
+    optind = 0;
+
+
+    while( (opt = getopt_long(argc, argv, short_opts, long_opts, NULL)) != -1 )
+    {
+        switch( opt )
+        {
+
+            case cmd_opt_help:
+                        puts(help_str);
+                        exit_if_not_daemonized(EXIT_SUCCESS);
+                        break;
+
+            case cmd_opt_version:
+                        puts(DAEMON_NAME "  version  " DAEMON_VERSION_STR "\n");
+                        exit_if_not_daemonized(EXIT_SUCCESS);
+                        break;
+
+
+                 //daemon options
+            case cmd_opt_no_chdir:
+                        daemon_info.no_chdir = 1;
+                        break;
+
+            case cmd_opt_no_fork:
+                        daemon_info.no_fork = 1;
+                        break;
+
+            case cmd_opt_no_close:
+                        daemon_info.no_close_stdio = 1;
+                        break;
+
+            case cmd_opt_pid_file:
+                        daemon_info.pid_file = optarg;
+                        break;
+
+            case cmd_opt_log_file:
+                        daemon_info.log_file = optarg;
+                        break;
+
+            case cmd_opt_cmd_pipe:
+                        daemon_info.cmd_pipe = optarg;
+                        break;
+
+            default:
+                        puts("for more detail see help\n\n");
+                        exit_if_not_daemonized(EXIT_FAILURE);
+                        break;
         }
-        handle_client(client_fd);
+    }
+}
 
-        current_time = time(NULL);
-        tm_ptr = localtime(&current_time);
+
+
+void* cmd_pipe_thread(void *thread_arg)
+{
+    int   fd;
+    int   argc;
+    char *arg;
+    char **argv;
+    char *cmd_pipe_buf;
+
+
+
+    pthread_detach(pthread_self());
+    unlink(daemon_info.cmd_pipe);
+
+
+    argv = (char **)malloc(PIPE_BUF*sizeof(char *));
+    if( !argv )
+        daemon_error_exit("Can't get mem for argv (CMD_PIPE): %m\n");
+
+
+    cmd_pipe_buf = (char *)malloc(PIPE_BUF);
+    if( !cmd_pipe_buf )
+        daemon_error_exit("Can't get mem for cmd_pipe_buf: %m\n");
+
+
+    if( mkfifo(daemon_info.cmd_pipe, 0622) != 0 )
+        daemon_error_exit("Can't create CMD_PIPE: %m\n");
+
+
+    fd = open(daemon_info.cmd_pipe, O_RDWR);
+    if( fd == -1 )
+        daemon_error_exit("Can't open CMD_PIPE: %m\n");
+
+
+
+    while(1)
+    {
+        memset(cmd_pipe_buf, 0, PIPE_BUF);
+
+
+        if( read(fd, cmd_pipe_buf, PIPE_BUF) == -1 )             // wait for command from DAEMON_CMD_PIPE_NAME
+            daemon_error_exit("read CMD_PIPE return -1: %m\n");
+
+
+        argc = 1;                                                // see getopt_long function
+        arg  = strtok(cmd_pipe_buf, " \t\n");
+
+
+        while( (arg != NULL)  && (argc < PIPE_BUF) )
+        {
+            argv[argc++] = arg;
+            arg          = strtok(NULL, " \t\n");
+        }
+
+
+        if( argc > 1 )
+          processing_cmd(argc, argv);
     }
 
-    close(server_fd);
-    unlink(SOCKET_PATH);
-    return 0;
+
+    return NULL;
+}
+
+
+
+void init(void *data)
+{
+    init_signals();
+
+
+    if( pthread_create(&pthread_cmd_pipe, NULL, cmd_pipe_thread, NULL) != 0 )
+       daemon_error_exit("Can't create thread_cmd_pipe: %m\n");
+
+
+    //Here is your code to initialize
+}
+
+#define debug_x_len 2560
+#define debug_y_len 1440
+#define ws2812_len  180
+
+typedef union {
+    u_int8_t R;
+    u_int8_t G;
+    u_int8_t B;
+} LED_RGB;
+
+int main(int argc, char *argv[])
+{
+    processing_cmd(argc, argv);
+    daemonize2(init, NULL);
+
+    Display *display = XOpenDisplay(NULL);
+    if (!display) {
+        fprintf(stderr, "Не удалось открыть дисплей\n");
+        return 1;
+    }
+
+    int screen = DefaultScreen(display);
+    Window root = RootWindow(display, screen);
+
+    // Получаем параметры экрана
+    XWindowAttributes attr;
+    XGetWindowAttributes(display, root, &attr);
+    int width = attr.width;
+    int height = attr.height;
+    int depth = attr.depth; // Глубина цвета
+
+    printf("Размер экрана: %dx%d, Глубина: %d бит\n", width, height, depth);
+
+
+    // Настройка XShm
+    XShmSegmentInfo shminfo;
+    XImage *image = XShmCreateImage(display, DefaultVisual(display, screen), depth, ZPixmap, NULL, &shminfo, width, height);
+    if (!image) {
+        fprintf(stderr, "Не удалось создать XImage\n");
+        XCloseDisplay(display);
+        return 1;
+    }
+
+    shminfo.shmid = shmget(IPC_PRIVATE, image->bytes_per_line * image->height, IPC_CREAT | 0777);
+    if (shminfo.shmid < 0) {
+        perror("shmget");
+        XDestroyImage(image);
+        XCloseDisplay(display);
+        return 1;
+    }
+
+    shminfo.shmaddr = (char *)shmat(shminfo.shmid, NULL, 0);
+    image->data = shminfo.shmaddr;
+    shminfo.readOnly = False;
+
+
+    if (!XShmAttach(display, &shminfo)) {
+        fprintf(stderr, "Не удалось прикрепить сегмент разделяемой памяти\n");
+        shmdt(shminfo.shmaddr);
+        shmctl(shminfo.shmid, IPC_RMID, NULL);
+        XDestroyImage(image);
+        XCloseDisplay(display);
+        return 1;
+    }
+
+    LED_RGB led_buf[ws2812_len] = {0,};
+    size_t x = 0, y = 0;
+    while( !daemon_info.terminated )
+    {
+        XShmGetImage(display, root, image, 0, 0, AllPlanes);
+        for(size_t pixel = 0; pixel < ws2812_len; pixel++) {
+            if(x <= debug_x_len && y == 0) {
+                x = (debug_x_len / ws2812_len) * pixel;
+            } else if (x >= 0 && y < debug_y_len) {
+                y = (debug_y_len / ws2812_len) * 
+            }
+
+            unsigned long pixel = XGetPixel(image, x, y);
+            unsigned long red_mask = image->red_mask;
+            unsigned long green_mask = image->green_mask;
+            unsigned long blue_mask = image->blue_mask;
+
+            unsigned char red = (pixel & red_mask) >> 16;
+            unsigned char green = (pixel & green_mask) >> 8;
+            unsigned char blue = (pixel & blue_mask);
+
+            led_buf[pixel].R = 
+        }
+        // Получение цвета пикселя
+        int x = 100; // Позиция X
+        int y = 100; // Позиция Y
+
+
+        // Извлечение RGB-значений из пикселя
+
+
+        printf("Цвет пикселя (RGB): (%u, %u, %u)\n", red, green, blue);
+        sleep(1);
+    }
+    XShmDetach(display, &shminfo);
+    shmdt(shminfo.shmaddr);
+    shmctl(shminfo.shmid, IPC_RMID, NULL);
+    XDestroyImage(image);
+    XCloseDisplay(display);
+
+    return EXIT_SUCCESS; // good job (we interrupted (finished) main loop)
 }
